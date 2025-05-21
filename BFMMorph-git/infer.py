d5cf2b80480ea6732e725da82c6908de3065c8dc
zihang5 <97323822+zihang5@users.noreply.github.com>
2025-05-21 09:51:53 +0800

    Create infer.py

---
+++ BFMMorph-git/infer.py
@@ -0,0 +1,108 @@
+import glob
+import os, utils
+from torch.utils.data import DataLoader
+from data import datasets, trans
+import numpy as np
+import torch
+from torchvision import transforms
+import time
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from natsort import natsorted
+import models
+import random
+import csv
+import BFMMorph
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+        self.vals = []
+        self.std = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+        self.vals.append(val)
+        self.std = np.std(self.vals)
+
+def main():
+    TIME = []
+
+    val_dir = 'IXI-data-path'
+    altas = 'altas-path'
+    model_folder = 'best/'
+    model_idx = -1
+    model_dir = 'experiments/' + model_folder
+    
+    
+    img_size = (160, 192, 224)
+    model = BFMMorph.BFMMorph(config = BFMMorph.get_3DBFMMorph_config())
+    model.load_state_dict(best_model)
+    model.cuda()
+
+    reg_model = utils.register_model(img_size, 'nearest')
+    reg_model.cuda()
+
+    test_composed = transforms.Compose([trans.Seg_norm(),
+                                        trans.NumpyType((np.float32, np.int16)),
+                                        ])
+    test_set = datasets.IXIBrainInferDataset(glob.glob(val_dir + '*.pkl'), altas, transforms=test_composed)
+    test_loader = DataLoader(test_set, batch_size=1, shuffle=False, num_workers=0, pin_memory=True, drop_last=True)
+
+    eval_dsc_def = AverageMeter()
+    eval_dsc_raw = AverageMeter()
+    eval_det = AverageMeter()
+    with torch.no_grad():
+        stdy_idx = 0
+        for data in test_loader:
+            model.eval()
+            data = [t.cuda() for t in data]
+            x = data[0]
+            y = data[1]
+            x_seg = data[2]
+            y_seg = data[3]
+    
+            x_def, flow = model(x, y)
+
+    
+            def_out = reg_model([x_seg.cuda().float(), flow.cuda()])
+            tar = y.detach().cpu().numpy()[0, 0, :, :, :]
+            jac_det = utils.jacobian_determinant_vxm(flow.detach().cpu().numpy()[0, :, :, :, :])
+    
+            eval_det.update(np.sum(jac_det <= 0) / np.prod(tar.shape), x.size(0))
+            dscs, dsc_trans = utils.dice_val_VOI(def_out.long(), y_seg.long())
+            dsc_raw = utils.dice_val_VOI(x_seg.long(), y_seg.long())
+
+            print('Trans dsc: {:.4f}, Raw dsc: {:.4f}'.format(dsc_trans.item(), dsc_raw.item()))
+            eval_dsc_def.update(dsc_trans.item(), x.size(0))
+            eval_dsc_raw.update(dsc_raw.item(), x.size(0))
+            stdy_idx += 1
+
+        print('Deformed DSC: {:.3f} +- {:.3f}, Affine DSC: {:.3f} +- {:.3f}'.format(eval_dsc_def.avg,
+                                                                                    eval_dsc_def.std,
+                                                                                    eval_dsc_raw.avg,
+                                                                                    eval_dsc_raw.std))
+        print('Deformed det: {}, std: {}'.format(eval_det.avg, eval_det.std))
+        
+
+if __name__ == '__main__':
+    GPU_iden = 0
+    GPU_num = torch.cuda.device_count()
+    print('Number of GPU: ' + str(GPU_num))
+    for GPU_idx in range(GPU_num):
+        GPU_name = torch.cuda.get_device_name(GPU_idx)
+        print('     GPU #' + str(GPU_idx) + ': ' + GPU_name)
+    torch.cuda.set_device(GPU_iden)
+    GPU_avai = torch.cuda.is_available()
+    print('Currently using: ' + torch.cuda.get_device_name(GPU_iden))
+    print('If the GPU is available? ' + str(GPU_avai))
+    main()
